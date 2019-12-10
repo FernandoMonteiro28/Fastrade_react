de11e086a9b2206f4b3cb92e7a784253779988d0
Joselaine Romão <55799037+jrscreateart@users.noreply.github.com>
2019-12-10 11:26:46 -0200

    cadastro produto

--- fastrade_react/src/pages/cadastroProduto/cadastroProduto.js
+++ fastrade_react/src/pages/cadastroProduto/cadastroProduto.js
@@ -1,142 +1,152 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import '../../assets/css/CadastroProduto.css';
 
 
-class cadastroProduto extends Component{
+class cadastroProduto extends Component {
     //Alteramos o estado de um elemento através do construtor 
-    constructor(props){
+    constructor(props) {
         //Passamos o props para o componente com o super
         super(props);
         this.state = {
 
-        listarProd:[],
+            listarProd: [],
 
-        //Criamos um obj com o estado inicial do nosso form
-            nomeProd:'',
-            precoProd:'',
-            categoriaId:'',
-            descProd:'',
-            valProd:''
+            //Criamos um obj com o estado inicial do nosso form
+            nomeProd: '',
+            precoProd: '',
+            categoriaId: '',
+            descProd: '',
+            valProd: ''
         }
-      
+
     }
 
     //Ciclo de vida 
 
-    componentDidMount(){
+    componentDidMount() {
         console.log('Carregado');
         console.log(this.listarProd);
         this.listarProdutos();
     }
 
-            listarProdutos =() =>{
-                fetch('https://localhost:5001/api/Catproduto')
-                .then(response =>response.json())
-                .then(data => this.setState({ listarProd : data }))
-                .catch(error =>{
-                    console.log(error)
-                })
-            }
+    listarProdutos = () => {
+        fetch('https://localhost:5001/api/Catproduto')
+            .then(response => response.json())
+            .then(data => this.setState({ listarProd: data }))
+            .catch(error => {
+                console.log(error)
+            })
+    }
 
 
-    
-    render(){
-    
-    
-        
+
+    render() {
+
+
+
         //  const{nomeProd,PrecoProd,descProd,valProd} = this.state;
-        return(
+        return (
             <div className="container">
-            <div className="card">
-         
-          <h1>Cadastro de Produto</h1>
+                <div className="card">
 
-            <div className="form-group">
-                <label htmlFor="nomeProd">Nome do Produto</label>
-                <div className="input-button">
-                    <input 
-                    id="nomeProd" 
-                    type="text"
-                    name = "NomeProd"
-                   //  value ={nomeProd}
-                   // onChange ={this.escutadorDeInput}
-                    />
-                </div>
-            </div>
+                    <h1 className="titulo_cadastro">Cadastro de Produto</h1>
 
-            <div className="form-group">
-                <label htmlFor="precoProd">Preço do Produto</label>
-                <div className="input-button">
-                    <input id="precoProd"
-                     type="number"
-                     name = "PrecoProd"
-                    //   value ={PrecoProd}
-                  //  onChange = {this.escutadorDeInput}
-                     />
-                </div>
-            </div>
+                    <div className="conjunto_cadastro">
+                        <div className="form-group">
+                            <label htmlFor="nomeProd">Nome do Produto</label>
+                            <div className="input-button">
+                                <input
+                                    id="nomeProd"
+                                    type="text"
+                                    name="NomeProd"
+                                //  value ={nomeProd}
+                                // onChange ={this.escutadorDeInput}
+                                />
+                            </div>
+                        </div>
 
-            <div className="form-group">
-                <label htmlFor="catProd">Categoria do Produto</label>
-                <div className="input-button">
-                    <select id ="descProd"
-                    name ="categoriaId"
-                    onChange ="">
-                        {
-                            this.state.listarProd.map(function(c){
-                                return(
-                                    
-                                    <option key ={c.idCatProduto} value={c.idCatProduto}>
-                                       {c.tipo}
-                                    </option>
+                        <div className="form-group">
+                            <label htmlFor="precoProd">Preço do Produto</label>
+                            <div className="input-button">
+                                <input id="precoProd"
+                                    type="number"
+                                    name="PrecoProd"
+                                //   value ={PrecoProd}
+                                //  onChange = {this.escutadorDeInput}
+                                />
+                            </div>
+                        </div>
 
-                                )
-                            })
-                        }
-                        {/* <option value="">Enlatado</option>
+                        <div className="form-group">
+                            <label htmlFor="catProd">Categoria do Produto</label>
+                            <div className="input-button">
+                                <select id="descProd"
+                                    name="categoriaId"
+                                    onChange="">
+                                    {
+                                        this.state.listarProd.map(function (c) {
+                                            return (
+
+                                                <option key={c.idCatProduto} value={c.idCatProduto}>
+                                                    {c.tipo}
+                                                </option>
+
+                                            )
+                                        })
+                                    }
+                                    {/* <option value="">Enlatado</option>
                         <option value="">Opel</option>
                         <option value="">Audi</option> */}
-                       
-                      </select>
-                   
+
+                                </select>
+
+                            </div>
+                        </div>
+
+
+                        <div className="form-group">
+                            <label htmlFor="descProd">Descrição do Produto</label>
+                            <div className="input-button">
+                                <input id="descProd"
+                                    type="text"
+                                    name="DescProd"
+                                // value ={descProd}
+                                // onChange = {this.escutadorDeInput}
+                                />
+                            </div>
+
+                        </div>
+
+                        <div className="form-group">
+                            <label htmlFor="valProd">Validade do Produto</label>
+                            <div className="input-button">
+                                <input id="valProd"
+                                    type="date"
+                                    name="ValProd"
+                                // value ={valProd}
+                                //  onChange = {this.escutadorDeInput}
+                                />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="buttons-area">
+
+                        <button
+                            type="submit"
+                            onClick=""
+                            value="Submit"
+                            className="botao">ADICIONAR</button>
+
+                        <button
+                            type="submit"
+                            onClick=""
+                            value="Submit"
+                            className="botao">SAIR</button>
+                    </div>
+
+
                 </div>
             </div>
-
-            
-            <div className="form-group">
-                <label htmlFor="descProd">Descrição do Produto</label>
-                <div className="input-button">
-                    <input id="descProd"
-                     type="text"
-                     name = "DescProd"
-                    // value ={descProd}
-                   // onChange = {this.escutadorDeInput}
-                     />
-                </div>
-                
-            </div>
-
-            <div className="form-group">
-                <label htmlFor="valProd">Validade do Produto</label>
-                <div className="input-button">
-                    <input id="valProd" 
-                    type="date"
-                    name ="ValProd"
-                    // value ={valProd}
-                  //  onChange = {this.escutadorDeInput}
-                    />
-                </div>
-            </div>
-
-            <div className="buttons-area">
-                <button className="botao">Incluir</button>
-                <button className="botao">Alterar</button>
-                <button className="botao">Excluir</button>
-                <button className="botao">Sair</button>
-            </div>
-            
-        </div>
-    </div>
 
 
         );
