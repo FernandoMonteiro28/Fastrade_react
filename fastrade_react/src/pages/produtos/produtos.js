b53fc3f1b8f79d4230e2c44cff9026979474536d
FernandoMonteiro28 <55153352+FernandoMonteiro28@users.noreply.github.com>
2019-12-10 11:50:10 -0300

    home

--- fastrade_react/src/pages/produtos/produtos.js
+++ fastrade_react/src/pages/produtos/produtos.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Laranja from '../../assets/img/laranja.jpg';
 import Card from '../../assets/img/Agrupar 14.png';
 import Cabecalho from '../../components/cabecalho/cabecalho';
@@ -6,97 +6,110 @@ import Rodape from '../../components/rodape/Rodape';
 
 class Produtos extends Component {
     render() {
-        return(
+        return (
             <div>
-                <Cabecalho/>
-                 <div class="container_lista">
-            <div class="busca">
-                <h3 class="ordenar">Ordenar por:</h3>
-                <select name="" id="">
-                    <option value="">Selecione</option>
-                    <option value="">Menor Preço</option>
-                    <option value="">Maior preço</option>
-                    <option value="">Mais Vendidos</option>
-                    <option value="">Data de Vencimento</option>
-                    <option value="">Melhor Desconto</option>
-                </select>
-            </div>
-        </div>
-        <div class="container_cards">
-            <div class="card">
-                <div class="sub_card">
-                    <img src={Laranja} alt=""/>
-                    <p class="produtor">Qualyban</p>
-                    <p>Laranja Lima Orgânica Qualyban
-                        600g</p>
-                    <p class="dinheiro">R$ 45,00</p>
+                <Cabecalho />
+                <a href="#abrirModal">Open Modal</a>
+
+                <div id="abrirModal" class="modal">
+                    <!-- conteúdo do modal aqui -->
                 </div>
-                <a href="#" class="btn_1">ADICIONAR</a>
-            </div>
-            <div class="card">
-                <img src={Laranja} alt=""/>
-                <p class="produtor">Qualyban</p>
-                <p>Batata Baroa cozida
-                    Orgânica Nativa 1kg</p>
-                <p class="dinheiro">R$ 45,00</p>
-                <a href="#" class="btn_1">ADICIONAR</a>
-            </div>
-            <div class="card">
-                <img src={Laranja} alt=""/>
-                <p class="produtor">Qualyban</p>
-                <p>Batata Baroa cozida
-                    Orgânica Nativa 1kg</p>
-                <p class="dinheiro">R$ 45,00</p>
-                <a href="#" class="btn_1">ADICIONAR</a>
-            </div>
-            <div class="card">
-                <img src={Laranja} alt=""/>
-                <p class="produtor">Qualyban</p>
-                <p>Batata Baroa cozida
-                    Orgânica Nativa 1kg</p>
-                <p class="dinheiro">R$ 45,00</p>
-                <a href="#" class="btn_1">ADICIONAR</a>
-            </div>
-            <div class="card">
-                <img src={Laranja} alt=""/>
-                <p class="produtor">Qualyban</p>
-                <p>Batata Baroa cozida
-                    Orgânica Nativa 1kg</p>
-                <p class="dinheiro">R$ 45,00</p>
-                <a href="#" class="btn_1">ADICIONAR</a>
-            </div>
-            <div class="card">
-                <img src={Laranja} alt=""/>
-                <p class="produtor">Qualyban</p>
-                <p>Batata Baroa cozida
-                    Orgânica Nativa 1kg</p>
-                <p class="dinheiro">R$ 45,00</p>
-                <a href="#" class="btn_1">ADICIONAR</a>
-            </div>
-            <div class="card">
-                <img src={Laranja} alt=""/>
-                <p class="produtor">Qualyban</p>
-                <p>Batata Baroa cozida
-                    Orgânica Nativa 1kg</p>
-                <p class="dinheiro">R$ 45,00</p>
-                <a href="#" class="btn_1">ADICIONAR</a>
-            </div>
-            <div class="card">
-                <img src={Laranja} alt=""/>
-                <p class="produtor">Qualyban</p>
-                <p>Batata Baroa cozida
-                    Orgânica Nativa 1kg</p>
-                <p class="dinheiro">R$ 45,00</p>
-                <a href="#" class="btn_1">ADICIONAR</a>
-            </div>
-        </div>
-        <p class="vermais"><a href="" class="btn_1">Ver Mais</a></p>
+                <a href="#abrirModal">Open Modal</a>
 
-        <div align="center">
+                <div id="abrirModal" class="modal">
+                    <a href="#fechar" title="Fechar" class="fechar">x</a>
+                    <h2>Janela Modal</h2>
+                    <p>Esta é uma simples janela de modal.</p>
+                    <p>Você pode fazer qualquer coisa aqui, página de Login, pop-ups, ou formulários</p>
+                </div>
+                <div class="container_lista">
+                    <div class="busca">
+                        <h3 class="ordenar">Ordenar por:</h3>
+                        <select name="" id="">
+                            <option value="">Selecione</option>
+                            <option value="">Menor Preço</option>
+                            <option value="">Maior preço</option>
+                            <option value="">Mais Vendidos</option>
+                            <option value="">Data de Vencimento</option>
+                            <option value="">Melhor Desconto</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="container_cards">
+                    <div class="card">
+                        <div class="sub_card">
+                            <img src={Laranja} alt="" />
+                            <p class="produtor">Qualyban</p>
+                            <p>Laranja Lima Orgânica Qualyban
+                        600g</p>
+                            <p class="dinheiro">R$ 45,00</p>
+                        </div>
+                        <a href="#" class="btn_1">ADICIONAR</a>
+                    </div>
+                    <div class="card">
+                        <img src={Laranja} alt="" />
+                        <p class="produtor">Qualyban</p>
+                        <p>Batata Baroa cozida
+                    Orgânica Nativa 1kg</p>
+                        <p class="dinheiro">R$ 45,00</p>
+                        <a href="#" class="btn_1">ADICIONAR</a>
+                    </div>
+                    <div class="card">
+                        <img src={Laranja} alt="" />
+                        <p class="produtor">Qualyban</p>
+                        <p>Batata Baroa cozida
+                    Orgânica Nativa 1kg</p>
+                        <p class="dinheiro">R$ 45,00</p>
+                        <a href="#" class="btn_1">ADICIONAR</a>
+                    </div>
+                    <div class="card">
+                        <img src={Laranja} alt="" />
+                        <p class="produtor">Qualyban</p>
+                        <p>Batata Baroa cozida
+                    Orgânica Nativa 1kg</p>
+                        <p class="dinheiro">R$ 45,00</p>
+                        <a href="#" class="btn_1">ADICIONAR</a>
+                    </div>
+                    <div class="card">
+                        <img src={Laranja} alt="" />
+                        <p class="produtor">Qualyban</p>
+                        <p>Batata Baroa cozida
+                    Orgânica Nativa 1kg</p>
+                        <p class="dinheiro">R$ 45,00</p>
+                        <a href="#" class="btn_1">ADICIONAR</a>
+                    </div>
+                    <div class="card">
+                        <img src={Laranja} alt="" />
+                        <p class="produtor">Qualyban</p>
+                        <p>Batata Baroa cozida
+                    Orgânica Nativa 1kg</p>
+                        <p class="dinheiro">R$ 45,00</p>
+                        <a href="#" class="btn_1">ADICIONAR</a>
+                    </div>
+                    <div class="card">
+                        <img src={Laranja} alt="" />
+                        <p class="produtor">Qualyban</p>
+                        <p>Batata Baroa cozida
+                    Orgânica Nativa 1kg</p>
+                        <p class="dinheiro">R$ 45,00</p>
+                        <a href="#" class="btn_1">ADICIONAR</a>
+                    </div>
+                    <div class="card">
+                        <img src={Laranja} alt="" />
+                        <p class="produtor">Qualyban</p>
+                        <p>Batata Baroa cozida
+                    Orgânica Nativa 1kg</p>
+                        <p class="dinheiro">R$ 45,00</p>
+                        <a href="#" class="btn_1">ADICIONAR</a>
+                    </div>
+                </div>
+                <p class="vermais"><a href="" class="btn_1">Ver Mais</a></p>
 
-            <img src={Card} alt="" class="card2"/>
-        </div>
-        <Rodape/>
+                <div align="center">
+
+                    <img src={Card} alt="" class="card2" />
+                </div>
+                <Rodape />
             </div>
         );
     }
